e180cd0df834e07030f91a48a0df6a3f5628d033
sugi-san <90301727+sugi-san@users.noreply.github.com>
2022-06-19 12:41:45 +0900

    Update function.py

--- function.py
+++ function.py
@@ -83,7 +83,30 @@ def display_pic(folder):
     plt.show()
     plt.close()
 
+    
+# --- size adjust --- 
+import cv2
 
+def size_opt(file):
+    max_size = 1280
+    npyImage = cv2.imread(filename = file, flags = cv2.IMREAD_COLOR)
+    intWidth = npyImage.shape[1]
+    intHeight = npyImage.shape[0]
+
+    fltRatio = float(intWidth) / float(intHeight)
+    intWidth = min(int(max_size * fltRatio), max_size)
+    intHeight = min(int(max_size / fltRatio), max_size)
+
+    # make even
+    if not intWidth % 2 == 0:
+      intWidth +=1
+    if not intHeight % 2 ==0:
+      intHeight +=1
+
+    npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(file, npyImage)
+    
+    
 # --- reset_folder ---
 import shutil
 
